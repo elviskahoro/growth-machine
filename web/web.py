61ddb331be5d37137207f58de3c42dc191481be7
elvis kahoro <elvis@reflex.dev>
2024-12-01 15:46:29 -0800

    feat: Adding front chat to the main web page to test

--- web/web.py
+++ web/web.py
@@ -4,7 +4,22 @@ import reflex as rx
 
 from web.pages.index import page
 
-app = rx.App()
+FRONT_CHAT_SCRIPT_URL: str = "https://chat-assets.frontapp.com/v1/chat.bundle.js"
+FRONT_CHAT_SCRIPT: str = (
+    "window.FrontChat('init', {chatId: 'acc4659ea96842c32ee8c5a2b5de6bfa', useDefaultLauncher: true});"
+)
+app = rx.App(
+    head_components=[
+        rx.script(
+            src=FRONT_CHAT_SCRIPT_URL,
+            strategy="beforeInteractive",
+        ),
+        rx.script(
+            FRONT_CHAT_SCRIPT,
+            strategy="afterInteractive",
+        ),
+    ],
+)
 app.add_page(
     component=page,
     route="/",
